4f094191802d873b86f6d543ced81b47c4a2d5f2
JenkenK <jenkenkwok@gmail.com>
2020-10-02 16:38:02 +0100

    created base and index html files

--- db/order_food.sql
+++ db/order_food.sql
@@ -39,7 +39,7 @@ CREATE TABLE restaurants (
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY, -- this is my order number
     -- order_number INT,
-    order_timestamp TIMESTAMP,
+    order_timestamp TIMESTAMP DEFAULT NOW,
     customer_id SERIAL REFERENCES customers(id) ON DELETE CASCADE, 
     restaurant_id SERIAL REFERENCES restaurants(id) ON DELETE CASCADE
     --   review TEXT
