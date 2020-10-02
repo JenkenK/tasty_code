3d4f9b3929221f837d0db123f463e9729b72c2f0
JenkenK <jenkenkwok@gmail.com>
2020-10-02 15:59:16 +0100

    created customer class

---
+++ models/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__ (self, name, address, payment, phone_number, service, id=None):
+        self.name = name
+        self.address = address
+        self.payment = payment
+        self.phone_number = phone_number
+        self.service = service
