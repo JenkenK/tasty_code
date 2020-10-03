235315652e298470fcf6760864cd22de1b067a0c
JenkenK <jenkenkwok@gmail.com>
2020-10-03 19:45:36 +0100

    added create function in rest cat control

--- controllers/restaurant_category_controller.py
+++ controllers/restaurant_category_controller.py
@@ -10,9 +10,17 @@ def restaurant_category():
     restaurant_categories = restaurant_category_repo.select_all()
     return render_template("restaurant_category/index.html", restaurant_categories=restaurant_categories)
 
+
 @restaurant_category_blueprint.route("/restaurant_category/new")
 def new_restaurant_category():
     return render_template("restaurant_category/new.html")
 
 
-    
+@restaurant_category_blueprint.route("/restaurant_category", methods=["POST"])
+def create_restaurant_category():
+    category = request.form['category']
+    new_restaurant_category = RestaurantCategory(category)
+    restaurant_category_repo.save(new_restaurant_category)
+    return redirect("/restaurant_category")
+
+
