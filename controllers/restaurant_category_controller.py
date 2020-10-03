0d089bef931f2fa5eb973da7f68095b8c3c2d829
JenkenK <jenkenkwok@gmail.com>
2020-10-03 19:38:43 +0100

    added index.html for rest cat

--- controllers/restaurant_category_controller.py
+++ controllers/restaurant_category_controller.py
@@ -7,8 +7,8 @@ restaurant_category_blueprint = Blueprint("restaurant_category", __name__)
 
 @restaurant_category_blueprint.route("/restaurant_category")
 def restaurant_category():
-    restaurant_category = restaurant_category_repo.select_all()
-    return render_template("restaurant_category/index.html", restaurant_category=restaurant_category)
+    restaurant_categories = restaurant_category_repo.select_all()
+    return render_template("restaurant_category/index.html", restaurant_categories=restaurant_categories)
 
 @restaurant_category_blueprint.route("/restaurant_category/new")
 def new_restaurant_category():
