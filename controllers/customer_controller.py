64b7fd29ab6a5dcbf5b7082542f6d4aa33d73f5f
JenkenK <jenkenkwok@gmail.com>
2020-10-03 18:02:43 +0100

    added show html for cust and show in cust controller

--- controllers/customer_controller.py
+++ controllers/customer_controller.py
@@ -11,3 +11,7 @@ def customers():
     customers = customer_repositories.select_all()
     return render_template("/customers/index.html", customers = customers)
 
+@customers_blueprint.route("/customers/<id>")
+def show(id):
+    customer = customer_repositories.select(id)
+    return render_template("customers/show.html", customer = customer, orders = orders)
