4a1d53c9ea750daa9d35efc0e579b02629f6bc14
JenkenK <jenkenkwok@gmail.com>
2020-10-04 11:15:04 +0100

    added create rest route and new.html for new rest

--- controllers/restaurant_controller.py
+++ controllers/restaurant_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 
 from models.restaurant import Restaurant
+from models.cuisine import Cuisine
 import repositories.restaurant_repository as restaurant_repository
 import repositories.cuisine_repository as cuisine_repository
 import repositories.customer_repository as customer_repository
@@ -23,4 +24,17 @@ def new_restaurant():
 
 
 # CREATE
-# @restaurant_blueprint.route("/restaurants", methods=['POST'])
+@restaurant_blueprint.route("/restaurants", methods=['POST'])
+def create_restaurant():
+    name = request.form['name']
+    cuisine_id = request.form['cuisine_id']
+    address = request.form['address']
+    phone_number = request.form['phone_number']
+    availability = request.form['availability']
+
+    cuisine = cuisine_repository.select(cuisine_id)   
+
+    new_restaurant = Restaurant(name, address, phone_number, availability, cuisine)
+
+    restaurant_repository.save(new_restaurant)
+    return redirect('/restaurants')
