cb18ff722938d4a6ba8e66a72e275dac0b1fbc8f
JenkenK <jenkenkwok@gmail.com>
2020-10-05 16:16:09 +0100

    added update route in dish controller

--- controllers/dish_controller.py
+++ controllers/dish_controller.py
@@ -25,12 +25,23 @@ def new_dish():
 def create_dish():
     pass
 
+
 # EDIT
 @dish_blueprint.route("/dishes/<id>/edit")
 def edit_dish(id):
     dish = dish_repository.select(id)
     return render_template('dishes/edit.html', dish = dish)
 
-    
+
 # UPDATE
+@dish_blueprint.route("/dishes/<id>", methods=['POST'])
+def update_dish(id):
+    name = request.form["name"]
+    price = request.form["price"]
+    description = request.form["description"]
+    update_dish = Dish(cuisine, id)
+    dish_repository.update(update_dish)
+    return redirect("/dishes")
+
+
 # DELETE
