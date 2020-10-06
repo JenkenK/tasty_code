4721ab06c4c454b79aaba547ca92c8a206b45a3c
JenkenK <jenkenkwok@gmail.com>
2020-10-06 13:51:51 +0100

    added dishes to order class

--- models/order.py
+++ models/order.py
@@ -1,6 +1,7 @@
 class Order():
-    def __init__(self, order_timestamp, customer, restaurant, id=None):
+    def __init__(self, order_timestamp, customer, restaurant, dishes, id=None):
         self.order_timestamp = order_timestamp
         self.customer = customer
         self.restaurant = restaurant
+        self.dishes = dishes
         self.id = id
