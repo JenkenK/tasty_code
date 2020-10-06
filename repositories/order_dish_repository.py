affcc3dacc7d1a6c909bc121998998481d0388a9
JenkenK <jenkenkwok@gmail.com>
2020-10-06 14:04:52 +0100

    added save function in order_dish repo

--- repositories/order_dish_repository.py
+++ repositories/order_dish_repository.py
@@ -3,4 +3,12 @@ from db.run_sql import run_sql
 from models.order_dish import OrderDish
 
 import repositories.order_dish_repository as order_dish_repository
-import repositories.dish_repository as dish_repositorys
+import repositories.dish_repository as dish_repository
+
+
+def save(order_dish):
+    sql = "INSERT INTO order_dishes (order_id, dish_id) VALUES (%s, %s) RETURNING *"
+    values = [order_dish.order.id, order_dish.dish.id]
+    results = run_sql(sql, values)
+    order_dish.id = results[0]['id']
+    return order_dish
